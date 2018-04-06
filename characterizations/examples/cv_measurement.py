3f6711aac67f4bbff4a7b35b72897dd05f263b28
Gijs <gijs.de.l@gmail.com>
2018-04-06 14:00:14 +0200

    updated example

--- characterizations/examples/cv_measurement.py
+++ characterizations/examples/cv_measurement.py
@@ -14,8 +14,8 @@ print('default npoints = %s'%cvm.NPOINTS)
 print('default C_p  = %s pF'%(cvm.C_P*1e12))
 
 
-Vgs_sim = np.linspace(-5,5,41)  
-cv_dev = cvm.CV_measurement('dummy_CV_mmt', Vgs_sim, station, 'dummy_lockin') 
+Vgs_sim = np.linspace(-5,5,11)  
+cv_dev = cvm.CV_measurement('dummy_CV_mmt', Vgs_sim, station, 'dummy_lockin', datapath = cvm.DATAPATH) 
 
 cv_dev.calibrate() # do this only when you change the setup
 cvm.C_P = cv_dev.C_p # this sets the dfault value of the module to the measured C_p for measurement of subsequent devices, this will be lost when the module is reloaded and set back to 500 pF
