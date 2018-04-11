14b6d6ed0f311db947fea25358e02c913ba89e7b
jdwatson <j.d.watson@tudelft.nl>
2018-04-11 18:01:14 +0200

    commit after snd round of mmts

--- characterizations/examples/cv_measurement.py
+++ characterizations/examples/cv_measurement.py
@@ -1,21 +1,36 @@
+import numpy as np
 from characterizations import CV_measurements as cvm
 from characterizations.CV_measurements import dummy_lockin
 import qcodes as qc
+
+
+from qcodes.instrument_drivers.stanford_research.SR865 import SR865
+
+
 try:
-    s= cvm.dummy_lockin('dummy_lockin', 'addr')
-    station = qc.Station(s)
+   lockin = SR865("lockin", "USB0::0xB506::0x2000::003225::INSTR")
+   #cvm.DATAPATH(r"C:\Users\jowat\data")
+   station = qc.Station(lockin)
+   
+    
 except:
     pass # instrument already created
 #s.frequency(1)
-
+ 
 
 print('default fmin, fmax = %s'%cvm.FREQUENCIES)
 print('default npoints = %s'%cvm.NPOINTS)
 print('default C_p  = %s pF'%(cvm.C_P*1e12))
 
 
-Vgs_sim = np.linspace(-5,5,11)  
-cv_dev = cvm.CV_measurement('dummy_CV_mmt', Vgs_sim, station, 'dummy_lockin', datapath = cvm.DATAPATH) 
+Vgs = np.linspace(-5,5,11)  
+cv_dev = cvm.CV_measurement('MOSCAP003', Vgs, station, 'lockin', datapath = r"C:\Users\jowat\data")
+
+#****USER INPUTS**********
+cv_dev.data['diameter'] = 50e-6
+cv_dev.data['column'] = 3
+cv_dev.data['thickness'] = 41e-9
+#*****END USER INPUTS*******
 
 cv_dev.calibrate() # do this only when you change the setup
 cvm.C_P = cv_dev.C_p # this sets the dfault value of the module to the measured C_p for measurement of subsequent devices, this will be lost when the module is reloaded and set back to 500 pF
